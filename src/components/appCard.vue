2cd3ade24adacb5307d58591a21c8190147a2880
casalaspro <casalaspro.alessandro@gmail.com>
2024-04-11 15:51:22 +0200

    ADD - db.json in assets and new component: aooCard.vue

---
+++ src/components/appCard.vue
@@ -0,0 +1,30 @@
+<template>
+
+
+
+
+</template>
+
+
+<script>
+
+export default {
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+    
+  },
+  props:{
+
+  },
+}
+
+
+</script>
+
+<style lang="scss" scope>
+
+</style>
